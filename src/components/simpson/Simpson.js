4d97f7bc17819210222912f8e2652cc0906ae3dd
Dem0nhunt3R <d3m0nhunt3r92@gmail.com>
2022-09-13 23:25:00 +0300

    lesson 1 simpsons

--- src/components/simpson/Simpson.js
+++ src/components/simpson/Simpson.js
@@ -1,11 +1,12 @@
 import classes from './Simpson.module.css'
 
 const Simpson = ({simpson}) => {
+    let {name,info,img} = simpson;
     return (
         <div className={classes.card}>
-            <h2>{simpson.name}</h2>
-            <img src={simpson.img} alt=""/>
-            <p>{simpson.info}</p>
+            <h2>{name}</h2>
+            <img src={img} alt={name}/>
+            <p>{info}</p>
         </div>
     );
 };
