c455b4e7766e40e4bfae0e5cd044bd32c1dc8128
Dem0nhunt3R <d3m0nhunt3r92@gmail.com>
2022-09-16 22:30:18 +0300

    lesson 2 ships

--- src/components/ship/Ship.js
+++ src/components/ship/Ship.js
@@ -2,6 +2,7 @@ import classes from './Ship.module.css'
 
 const Ship = ({ship}) => {
     const {mission_name, launch_year,links: {mission_patch_small}} = ship;
+
     return (
         <div className={classes.card}>
             <p>{mission_name}. Year: {launch_year}</p>
