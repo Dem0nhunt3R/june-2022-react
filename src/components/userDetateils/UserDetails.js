d1667d37e2c0a7e89408802ddcea72066a467bba
Dem0nhunt3R <d3m0nhunt3r92@gmail.com>
2022-09-16 23:05:21 +0300

    lesson 2 users

--- src/components/userDetateils/UserDetails.js
+++ src/components/userDetateils/UserDetails.js
@@ -1,22 +1,14 @@
 import classes from './UserDetails.module.css'
 
 const UserDetails = ({userInfo}) => {
-    console.log(userInfo[0].id)
-
     return (
         <div className={classes.infoBox}>
             {
-                userInfo && userInfo.map(item => {
-                    let a = '';
-                    let b = '';
-                    for (const key in item) {
-                        a = key;
-                        b = item[key];
-                        // return <p className={classes.par}>{key}: {item[key]}</p>;
-                    }
-                    return <p key={a} className={classes.par}>{a}: {b}</p>
-                })
-
+                userInfo && userInfo.map(item =>
+                    <p key={Object.keys(item).toString()}
+                       className={classes.par}>{Object.keys(item)}: {Object.values(item)}
+                    </p>
+                )
             }
         </div>
     );
